0454606dbbdbec59e7de8f096d2ab104ae4ac600
codeantu@gmail.com <codeAbinash@gmail.com>
2023-10-02 00:32:38 +0530

    fer

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-
 let input = getID("input")
 let key = getID("key")
-
-
+let copy = getID("copy")
+let clear = getID("clear")
 
 input.addEventListener("keyup", encrypt)
 key.addEventListener("keyup", encrypt)
+copy.addEventListener("click", textCopy)
+clear.addEventListener("click", textClear)
 
 function encrypt() {
    let text = getID("input").value
@@ -15,8 +16,12 @@ function encrypt() {
    let temp
 
    if (key != "") {
+      let x = '0'
+      for(let i = 0; i < key.length; i++){
+         x ^= key.charCodeAt(i)
+      }
       for (let i = 0; i < text.length; i++) {
-         temp = text.charCodeAt(i) ^ key.charCodeAt(i % key.length)
+         temp = text.charCodeAt(i) ^ key.charCodeAt(i % key.length) ^ x
          encrypted += String.fromCharCode(temp)
       }
    }
@@ -24,18 +29,26 @@ function encrypt() {
 
 }
 
-// function decrypt() {
-//    let text = getID("text").value
-//    let key = getID("key").value
-//    let output = getID("output")
-//    let decrypted = ""
-//    if (text == "" || key == "") {
-//       alert("Please enter a message and a key")
-//    } else {
-//       decrypted = CryptoJS.AES.decrypt(text, key).toString(CryptoJS.enc.Utf8)
-//       output.value = decrypted
-//    }
-// }
+function textCopy() {
+   let output = getID("output")
+   let input = getID("input")
+   output.select()
+   output.setSelectionRange(0, 99999)
+   document.execCommand("copy")
+   input.focus()
+
+}
+
+function textClear() {
+   let output = getID("output")
+   let input = getID("input")
+   let key = getID("key")
+   output.value = ""
+   input.value = ""
+   key.value = ""
+   input.focus()
+}
+
 
 function getID(id) {
    return document.getElementById(id)
